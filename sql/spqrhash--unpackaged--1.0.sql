0d85fc029ccdbabbadb4e803f173d067ce6184f7
Yury Frolov <57130330+EinKrebs@users.noreply.github.com>
2025-08-21 11:05:40 +0500

    Rename hash funcitons (#2)

--- sql/spqrhash--unpackaged--1.0.sql
+++ sql/spqrhash--unpackaged--1.0.sql
@@ -1,9 +1,4 @@
 
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(text, text);
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(bytea, text);
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(text, text, int8);
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(bytea, text, int8);
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(text, text, int8, int8);
-ALTER EXTENSION spqrhash ADD FUNCTION hash64_string(bytea, text, int8, int8);
-ALTER EXTENSION spqrhash ADD FUNCTION hash_int8(int8, text);
-
+ALTER EXTENSION spqrhash ADD FUNCTION spqrhash_murmur3(int8);
+ALTER EXTENSION spqrhash ADD FUNCTION spqrhash_murmur3(text);
+ALTER EXTENSION spqrhash ADD FUNCTION spqrhash_murmur3(bytea);
--- sql/uninstall_spqrhash.sql
+++ sql/uninstall_spqrhash.sql
@@ -1,9 +1,5 @@
 
-DROP FUNCTION hash64_string(text, text);
-DROP FUNCTION hash64_string(bytea, text);
-DROP FUNCTION hash64_string(text, text, int8);
-DROP FUNCTION hash64_string(bytea, text, int8);
-DROP FUNCTION hash64_string(text, text, int8, int8);
-DROP FUNCTION hash64_string(bytea, text, int8, int8);
-DROP FUNCTION hash_int8(int8, text);
+DROP FUNCTION spqrhash_murmur3(int8);
+DROP FUNCTION spqrhash_murmur3(text);
+DROP FUNCTION spqrhash_murmur3(bytea);
 
