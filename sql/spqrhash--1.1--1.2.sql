e3ad79ec493517af3b170452e4c08f6970460aac
Yury Frolov <yura200253@gmail.com>
2026-07-03 19:08:18 +0500

    Add city32 function too

--- sql/spqrhash--1.1--1.2.sql
+++ sql/spqrhash--1.1--1.2.sql
@@ -6,4 +6,10 @@ SELECT spqrhash_murmur3(id::text)
 $$
 LANGUAGE SQL;
 
+CREATE OR REPLACE FUNCTION spqrhash_city32(id uuid)
+RETURNS int8 AS
+$$
+SELECT spqrhash_city32(id::text)
+$$
+LANGUAGE SQL;
 
