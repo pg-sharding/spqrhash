c9c2257ee8c9456159e8620bbfb651eba2e468f4
Yury Frolov <yura200253@gmail.com>
2026-07-06 14:22:28 +0500

    schema-qualify functions in declarations

--- sql/spqrhash--1.1--1.2.sql
+++ sql/spqrhash--1.1--1.2.sql
@@ -2,14 +2,14 @@
 CREATE OR REPLACE FUNCTION spqrhash_murmur3(id uuid)
 RETURNS int8 AS
 $$
-SELECT spqrhash_murmur3(id::text)
+SELECT public.spqrhash_murmur3(id::text)
 $$
 LANGUAGE SQL;
 
 CREATE OR REPLACE FUNCTION spqrhash_city32(id uuid)
 RETURNS int8 AS
 $$
-SELECT spqrhash_city32(id::text)
+SELECT public.spqrhash_city32(id::text)
 $$
 LANGUAGE SQL;
 
